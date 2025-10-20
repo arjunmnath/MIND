fixed masking issues in multi head self attention

training/dryrun.py
@@ -1,119 +1,47 @@
-import os
-import sys
 from pathlib import Path
 
-import hydra
-import mlflow
 import torch
-from config_classes import DataConfig, MLFlowConfig, OptimizerConfig, TrainingConfig
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.tensorboard import SummaryWriter
+from tqdm import tqdm
+
 from dataset import Mind
 from models import *
-from models.models import InfoNCE, TwoTowerRecommendation
-from omegaconf import DictConfig
-from torch.utils.data import DataLoader
-from torchinfo import summary
-from torchmetrics.retrieval import RetrievalAUROC, RetrievalNormalizedDCG
-from trainer import Trainer
-from utils import create_optimizer
+from utils import plot_attention_scores
 
+writer = SummaryWriter(log_dir="runs/experiment_name")
 
-def verify_min_gpu_count(min_gpus: int = 1) -> bool:
-    """
-    Verifies if there are enough GPUs available for training.
-
-    Args:
-        min_gpus (int): Minimum number of GPUs required.
-
-    Returns:
-        bool: True if the required number of GPUs is available, False otherwise.
-    """
-    has_gpu = torch.backends.mps.is_available()
-    gpu_count = torch.mps.device_count()
-    return has_gpu and gpu_count >= min_gpus
-
-
-def get_train_objs(data_cfg: DataConfig, opt_cfg: OptimizerConfig):
-    """
-    Initializes training objects including datasets, model, loss function, and metrics.
-
-    Args:
-        data_cfg (DataConfig): Configuration object containing data-related settings.
-        opt_cfg (OptimizerConfig): Configuration object containing optimizer-related settings.
-
-    Returns:
-        tuple: Contains the model, optimizer, loss function, metrics, training dataset, and testing dataset.
-    """
-    data_dir = Path(data_cfg.data_dir)
-    embed_dir = Path(data_cfg.embed_dir)
-    train_dataset = Mind(
-        dataset_dir=data_dir / "train",
-        precompute=data_cfg.precompute,
-        embed_dir=embed_dir / "train",
-    )
-    test_dataset = Mind(
-        dataset_dir=data_dir / "test",
-        precompute=data_cfg.precompute,
-        embed_dir=embed_dir / "test",
-    )
-    loss_fn = InfoNCE()
-    auc_roc = RetrievalAUROC()
-    ndcg_5 = RetrievalNormalizedDCG(top_k=5)
-    ndcg_10 = RetrievalNormalizedDCG(top_k=10)
-    model = TwoTowerRecommendation()
-    optimizer = create_optimizer(model, opt_cfg)
-
-    return (
-        model,
-        optimizer,
-        loss_fn,
-        [auc_roc, ndcg_5, ndcg_10],
-        train_dataset,
-        test_dataset,
-    )
-
-
-@hydra.main(version_base=None, config_path=".", config_name="mind_train_cfg.yaml")
-def main(cfg: DictConfig):
-    # Use MPS device for training on Apple GPU
-    device = (
-        torch.device("mps")
-        if torch.backends.mps.is_available()
-        else torch.device("cpu")
-    )
-
-    # configs
-    opt_cfg = OptimizerConfig(**cfg["optimizer_config"])
-    data_cfg = DataConfig(**cfg["data_config"])
-    trainer_cfg = TrainingConfig(**cfg["trainer_config"])
-    mlflow_cfg = MLFlowConfig(**cfg["mlflow"])
-
-    if torch.backends.mps.is_available():
-        print("Using MPS for training")
-    else:
-        print("MPS not available, falling back to CPU")
-
-    model, optimizer, loss_fn, metrices, train_data, test_data = get_train_objs(
-        data_cfg, opt_cfg
-    )
-    trainer = Trainer(
-        config=trainer_cfg,
-        model=model,
-        loss_fn=loss_fn,
-        metrices=metrices,
-        optimizer=optimizer,
-        train_dataset=train_data,
-        test_dataset=test_data,
-    )
-
-    trainer.train()
-
-
-if __name__ == "__main__":
-    import os
-
-    os.environ["LOCAL_RANK"] = "0"
-    os.environ["RANK"] = "0"
-    if not verify_min_gpu_count(min_gpus=1):
-        print(f"Unable to locate sufficient 1 GPUs to run this example. Exiting.")
-        sys.exit()
-    main()
+model = TwoTowerRecommendation()
+optimizer = optim.AdamW(model.parameters(), lr=0.001)
+# h = torch.randn(1, 558, 768)
+# c = torch.randn(1, 35, 768)
+# nc = torch.randn(1, 297, 768)
+# h[:, 5:, :] = 0
+# c[:, 1:, :] = 0
+# nc[:, 2:, :] = 0
+[h, c, nc] = [val.unsqueeze(0) for _, val in torch.load("mock.pth", weights_only=False)]
+print(h.shape, c.shape, nc.shape)
+dataset = TensorDataset(h, c, nc)
+loader = DataLoader(dataset)
+num_epochs = 1000
+for epoch in range(num_epochs):
+    model.train()
+    optimizer.zero_grad()
+    for history, clicks, non_clicks in loader:
+        loss, user_repr, impressions, labels, attn_scores, seq_len = model(
+            history, clicks, non_clicks
+        )
+        # print(user_repr @ impressions.mT, labels, seq_len)
+        loss.backward()
+        for name, param in model.named_parameters():
+            if param.grad is not None:
+                print(name, param.grad.abs().max())  # Check the max gradient
+        optimizer.step()
+        if epoch == num_epochs - 1:
+            plot_attention_scores(
+                attn_scores,
+                title="Multi-Head Attention Scores",
+                non_padded_size=seq_len,
+            )

training/models/models.py
@@ -5,11 +5,57 @@ import torch.nn.functional as F
 from sentence_transformers import SentenceTransformer
 from torchinfo import summary
 
-try:
-    from .user_encoder import UserEncoder
+# try:
+#     from .user_encoder import UserEncoder
+#
+# except ImportError:
+#     from user_encoder import UserEncoder
+#
 
-except ImportError:
-    from user_encoder import UserEncoder
+
+class UserEncoder(nn.Module):
+    """
+    Complete User Encoder module combining multi-head self-attention.
+    """
+
+    def __init__(self, news_embed_dim, num_heads=16):
+        super().__init__()
+
+        assert (
+            news_embed_dim % num_heads == 0
+        ), f"news_embed_dim ({news_embed_dim}) must be divisible by num_heads ({num_heads})"
+
+        # Define the multi-head self-attention layer
+        self.attention = nn.MultiheadAttention(
+            embed_dim=news_embed_dim, num_heads=num_heads
+        )
+
+        # Optionally, use a linear layer to project the output to the desired representation size
+        self.fc = nn.Linear(news_embed_dim, news_embed_dim)
+
+    def forward(self, news_embeds: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            news_embeds: (batch_size, num_news, news_embed_dim)
+                        - embeddings of news browsed by users
+        Returns:
+            user_repr: (batch_size, news_embed_dim) - user representation
+            attention_weights: (batch_size, num_news) - news importance weights
+        """
+        news_embeds = news_embeds.transpose(
+            0, 1
+        )  # (num_news, batch_size, news_embed_dim)
+        attn_output, attn_weights = self.attention(
+            news_embeds,
+            news_embeds,
+            news_embeds,
+            need_weights=True,
+            average_attn_weights=False,
+        )
+        attn_output = attn_output * ((mask.mT.unsqueeze(2)).float())
+        user_repr = attn_output.mean(dim=0)
+        user_repr = self.fc(user_repr)
+        return user_repr, attn_weights
 
 
 class NewsEncoder(nn.Module):
@@ -24,7 +70,6 @@ class NewsEncoder(nn.Module):
         embed_dim (int): Dimension of the input embeddings (default 768).
         hidden_dim (int): Hidden dimension for the projection layer (default 128).
         dropout (float): Dropout rate (default 0.1).
-        is_vector_input (bool): Whether the input is pre-encoded vectors (default True).
 
     Methods:
         forward(contents): Encodes and projects the input, returning normalized vectors.
@@ -33,13 +78,13 @@ class NewsEncoder(nn.Module):
     def __init__(self, embed_dim=768, dropout=0.1):
         super(NewsEncoder, self).__init__()
         self.project = nn.Sequential(
-            nn.Linear(embed_dim, embed_dim, bias=False),
+            nn.Linear(embed_dim, embed_dim),
             nn.Dropout(dropout),
             nn.LayerNorm(embed_dim),
             nn.ReLU(),
         )
 
-    def forward(self, contents: torch.Tensor) -> torch.Tensor:
+    def forward(self, contents: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
         """
         Encodes and projects the input into a normalized vector.
         Args:
@@ -48,7 +93,8 @@ class NewsEncoder(nn.Module):
             torch.Tensor: Normalized projected vectors.
         """
         projections = self.project(contents)  # shape: [batch_size, 768]
-        return F.normalize(projections, p=2, dim=-1)
+        projections = projections * mask.unsqueeze(2).float()
+        return F.normalize(contents + projections, p=2, dim=-1)
 
 
 class TwoTowerRecommendation(nn.Module):
@@ -57,6 +103,8 @@ class TwoTowerRecommendation(nn.Module):
         self.user_tower = UserEncoder(768)
         self.news_tower = NewsEncoder()
         self._cosine_loss = nn.CosineEmbeddingLoss()
+        self._infonce = InfoNCE()
+        self.apply(self.init_weights)
 
     def forward(self, history, clicks, non_clicks):
         """
@@ -64,53 +112,61 @@ class TwoTowerRecommendation(nn.Module):
         clicks: [batch_size, clicks_pad_size, 768]
         non_clicks: [batch_size, non_clicks_pad_size, 768]
         """
-        history = self.news_tower(history)
-        clicks = self.news_tower(clicks)  # dim: [batch_size, clicks_pad_size, 768]
-        non_clicks = self.news_tower(non_clicks)
-        user_repr, _ = self.user_tower(history)  # dim: [batch_size, 768]
+        batch_size = clicks.shape[0]
+        history_mask = history.sum(dim=-1) != 0
+        click_mask = clicks.sum(dim=-1) != 0
+        non_click_mask = non_clicks.sum(dim=-1) != 0
+        seq_len = history_mask.long().sum().item()
+        click_seq_len = click_mask.long().sum().item()
+        non_click_seq_len = non_click_mask.long().sum().item()
+        history = self.news_tower(
+            history, mask=history_mask
+        )  # dim: [batch_size, seq_len, 768]
+        clicks = self.news_tower(
+            clicks, mask=click_mask
+        )  # dim: [batch_size, num_clicks, 768]
+        non_clicks = self.news_tower(
+            non_clicks, mask=non_click_mask
+        )  # dim: [batch_size, num_non_clicks, 768]
+        user_repr, attn_scores = self.user_tower(
+            history, mask=history_mask
+        )  # dim: [batch_size, 768]
         user_repr = F.normalize(
             user_repr.unsqueeze(1), p=2, dim=-1
         )  # dim: [batch_size, 1, 768]
-        click_pad_size = clicks.shape[1]
-        non_click_pad_size = non_clicks.shape[1]
-        labels_positive = torch.ones(
-            clicks.shape[0], clicks.shape[1], device=user_repr.device
-        )
+        labels_positive = torch.ones(batch_size, click_seq_len, device=user_repr.device)
         lables_negative = torch.full(
-            (non_clicks.shape[0], non_clicks.shape[1]), -1, device=user_repr.device
+            (batch_size, non_click_seq_len), -1, device=user_repr.device
         )
-        impressions = torch.cat([clicks, non_clicks], dim=1)
+        impressions = torch.cat(
+            [
+                clicks[click_mask].view(1, -1, 768),
+                non_clicks[non_click_mask].view(1, -1, 768),
+            ],
+            dim=1,
+        )
         labels = torch.cat([labels_positive, lables_negative], dim=1)
-        loss = self._cosine_loss(
-            user_repr.expand(-1, click_pad_size + non_click_pad_size, 768).reshape(
+        loss1 = self._cosine_loss(
+            user_repr.expand(-1, click_seq_len + non_click_seq_len, 768).reshape(
                 -1, 768
             ),
             impressions.reshape(-1, 768),
             labels.flatten(),
         )
+        # loss2 = self._infonce(user_repr, impressions, (labels + 1) / 2)
+        return (loss1, user_repr, impressions, labels, attn_scores, seq_len)
 
-        assert not torch.isnan(history).any(), "NaNs found in history"
-        assert not torch.isnan(clicks).any(), "NaNs found in clicks"
-        assert not torch.isnan(non_clicks).any(), "NaNs found in non_clicks"
-        assert not torch.isnan(loss).any(), "NaNs found in loss"
-        return (loss, user_repr, impressions, labels)
+    def init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            nn.init.xavier_uniform_(m.weight)
+            if m.bias is not None:
+                nn.init.zeros_(m.bias)
 
 
 class InfoNCE(nn.Module):
-    """Partial Implementation of the InfoNCE (Information Noise Contrastive Estimation) loss function.
-
-    Paper: https://arxiv.org/pdf/1807.03748#page=3
-
-    This loss function is commonly used in contrastive learning, where the goal is to
-    distinguish between similar and dissimilar pairs of data points. The loss encourages
-    the model to assign a higher similarity score to positive pairs and a lower similarity
-    score to negative pairs.
-
-    Args:
-        temperature (float, optional): A scaling factor that adjusts the distribution
-            of similarities. Default is 0.07. Lower values make the model more sensitive
-            to small differences in similarity, while higher values make the model more
-            lenient.
+    """
+    InfoNCE (Information Noise Contrastive Estimation) loss.
+    Paper: https://arxiv.org/abs/1807.03748
     """
 
     def __init__(self, temperature=0.5):
@@ -123,22 +179,30 @@ class InfoNCE(nn.Module):
         super(InfoNCE, self).__init__()
         self.temperature = temperature
 
-    def forward(self, similarities: torch.Tensor, target: torch.Tensor):
+    def forward(self, query: torch.Tensor, key: torch.Tensor, labels: torch.Tensor):
         """
         Compute the InfoNCE loss.
 
-        The loss is computed by scaling the similarity scores (logits) by the temperature
-        factor and applying binary cross-entropy loss between the logits and the target labels.
+        Assumes positive pairs are aligned by index (query[i] matches key[i])
 
         Args:
-            similarities (torch.Tensor): A flat tensor (1-D) containing the similarity scores (logits).
-            target (torch.Tensor): A flat tensor (1-D) containing the binary target labels (0 or 1).
-
+            query: Tensor of shape (batch_size, embedding_dim)
+            key: Tensor of shape (batch_size, embedding_dim)
+            labels: (batch_size, num_impressions) - binary matrix where 1=positive, 0=negative
         Returns:
             torch.Tensor: The computed InfoNCE loss.
+
         """
-        logits = similarities / self.temperature
-        loss = F.binary_cross_entropy_with_logits(logits, target)
+        query = F.normalize(query, dim=1)
+        key = F.normalize(key, dim=1)
+
+        logits = (query @ key.mT).squeeze(1)
+        # sigmoid_logits = (torch.sigmoid(logits) - 0.5) * 2
+        # sigmoid_logits = torch.clamp(sigmoid_logits, min=-1, max=1)
+        loss = -torch.mean(
+            labels.float() * torch.log(logits)
+            + (1 - labels.float()) * torch.log(1 - logits)
+        )
         return loss
 
 

training/single_gpu_training.py
@@ -6,7 +6,6 @@ import hydra
 import mlflow
 import torch
 from omegaconf import DictConfig
-from torch.distributed import destroy_process_group, init_process_group
 from torch.utils.data import DataLoader
 from torchinfo import summary
 from torchmetrics.retrieval import RetrievalAUROC, RetrievalNormalizedDCG
@@ -29,24 +28,9 @@ def verify_min_gpu_count(min_gpus: int = 1) -> bool:
     Returns:
         bool: True if the required number of GPUs is available, False otherwise.
     """
-    has_gpu = torch.accelerator.is_available()
-    gpu_count = torch.accelerator.device_count()
+    has_gpu = torch.backends.mps.is_available()
+    gpu_count = torch.mps.device_count()
     return has_gpu and gpu_count >= min_gpus
-
-
-def ddp_setup():
-    """
-    Sets up Distributed Data Parallel (DDP) for multi-GPU training.
-
-    This function initializes the process group and sets the device for each process.
-    """
-    acc = torch.accelerator.current_accelerator()
-    rank = int(os.environ["LOCAL_RANK"])
-    device: torch.device = torch.device(f"{acc}:{rank}")
-    backend = torch.distributed.get_default_backend_for_device(device)
-    init_process_group(backend=backend)
-    torch.accelerator.set_device_index(rank)
-    return device
 
 
 def get_train_objs(data_cfg: DataConfig, opt_cfg: OptimizerConfig):
@@ -66,11 +50,13 @@ def get_train_objs(data_cfg: DataConfig, opt_cfg: OptimizerConfig):
         dataset_dir=data_dir / "train",
         precompute=data_cfg.precompute,
         embed_dir=embed_dir / "train",
+        is_sanity_run=True,
     )
     test_dataset = Mind(
         dataset_dir=data_dir / "test",
         precompute=data_cfg.precompute,
         embed_dir=embed_dir / "test",
+        is_sanity_run=True,
     )
     loss_fn = InfoNCE()
     auc_roc = RetrievalAUROC()
@@ -78,7 +64,6 @@ def get_train_objs(data_cfg: DataConfig, opt_cfg: OptimizerConfig):
     ndcg_10 = RetrievalNormalizedDCG(top_k=10)
     model = TwoTowerRecommendation()
     optimizer = create_optimizer(model, opt_cfg)
-
     return (
         model,
         optimizer,
@@ -91,7 +76,12 @@ def get_train_objs(data_cfg: DataConfig, opt_cfg: OptimizerConfig):
 
 @hydra.main(version_base=None, config_path=".", config_name="mind_train_cfg.yaml")
 def main(cfg: DictConfig):
-    device = ddp_setup()
+    # Use MPS device for training on Apple GPU
+    device = (
+        torch.device("mps")
+        if torch.backends.mps.is_available()
+        else torch.device("cpu")
+    )
 
     # configs
     opt_cfg = OptimizerConfig(**cfg["optimizer_config"])
@@ -99,12 +89,10 @@ def main(cfg: DictConfig):
     trainer_cfg = TrainingConfig(**cfg["trainer_config"])
     mlflow_cfg = MLFlowConfig(**cfg["mlflow"])
 
-    rank = int(os.environ["LOCAL_RANK"])
-    if rank == 0:
-        mlflow.set_tracking_uri("databricks")
-        mlflow.set_experiment(
-            "/Users/dev.arjunmnath@gmail.com/mind-recommendation-system"
-        )
+    if torch.backends.mps.is_available():
+        print("Using MPS for training")
+    else:
+        print("MPS not available, falling back to CPU")
 
     model, optimizer, loss_fn, metrices, train_data, test_data = get_train_objs(
         data_cfg, opt_cfg
@@ -117,49 +105,10 @@ def main(cfg: DictConfig):
         optimizer=optimizer,
         train_dataset=train_data,
         test_dataset=test_data,
+        use_ddp=False,
     )
-    if rank == 0:
-        with mlflow.start_run() as run:
-            params = {
-                "learning_rate": opt_cfg.learning_rate,
-                "batch_size": trainer_cfg.batch_size,
-                "loss_function": loss_fn.__class__.__name__,
-                "metrics": [metric.__class__.__name__ for metric in metrices],
-                "optimizer": "AdamW",
-            }
-            mlflow.log_params(params)
-            click_padding = 35
-            history_padding = 558
-            non_click_padding = 297
-
-            with open("model_summary.txt", "w") as f:
-                f.write(
-                    str(
-                        summary(
-                            model,
-                            [
-                                (64, history_padding, 768),
-                                (64, click_padding, 768),
-                                (64, non_click_padding, 768),
-                            ],
-                            device=device,
-                        )
-                    )
-                )
-            mlflow.log_artifact("model_summary.txt")
-            trainer.train()
-            model_info = mlflow.pytorch.log_model(model, name="model")
-            print(model_info)
-    else:
-        trainer.train()
-    destroy_process_group()
+    trainer.train()
 
 
 if __name__ == "__main__":
-    _min_gpu_count = 1
-    if not verify_min_gpu_count(min_gpus=_min_gpu_count):
-        print(
-            f"Unable to locate sufficient {_min_gpu_count} gpus to run this example. Exiting."
-        )
-        sys.exit()
     main()

training/trainer.py
@@ -5,13 +5,15 @@ from typing import List, Tuple
 import fsspec
 import mlflow
 import torch
-from config_classes import Snapshot, TrainingConfig
-from models.models import InfoNCE
+import torch.distributed as dist
 from torch.amp import GradScaler, autocast
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.optim.lr_scheduler import LinearLR, SequentialLR
 from torch.utils.data import DataLoader, Dataset
 from torch.utils.data.distributed import DistributedSampler
+
+from config_classes import Snapshot, TrainingConfig
+from models.models import InfoNCE
 from utils import upload_to_s3
 
 
@@ -25,14 +27,14 @@ class Trainer:
         metrices,
         train_dataset: Dataset,
         test_dataset: Dataset,
+        use_ddp: bool = True,
     ):
         self.config = config
         self.model = model
         self.loss_fn = loss_fn
         self.metrices = metrices
-        self.local_rank = int(os.environ["LOCAL_RANK"])
-        self.global_rank = int(os.environ["RANK"])
-
+        self.local_rank = dist.get_rank() if use_ddp else 0
+        self.use_ddp = use_ddp
         self.acc = torch.accelerator.current_accelerator()
         self.device: torch.device = torch.device(f"{self.acc}:{self.local_rank}")
         self.device_type = self.device.type
@@ -63,7 +65,7 @@ class Trainer:
         if self.config.snapshot_path is None:
             self.config.snapshot_path = "snapshot.pt"
         self._load_snapshot()
-        if self.device_type == "cuda":
+        if self.device_type == "cuda" and self.use_ddp:
             self.model = DDP(self.model, device_ids=[self.local_rank])
             self.config.use_amp = True
         if self.config.use_amp:
@@ -77,7 +79,7 @@ class Trainer:
                 pin_memory=True,
                 shuffle=False,
                 num_workers=self.config.data_loader_workers,
-                sampler=DistributedSampler(dataset),
+                sampler=DistributedSampler(dataset) if self.use_ddp else None,
             )
             if (self.device_type == "cuda")
             else DataLoader(
@@ -130,7 +132,7 @@ class Trainer:
             dtype=torch.float16,
             enabled=(self.config.use_amp),
         ):
-            loss, user_repr, impressions, labels = self.model(
+            loss, user_repr, impressions, labels, attn_score, seq_len = self.model(
                 history, clicks, non_clicks
             )
             indexes, relevance, target = self._prepare_for_metrics(
@@ -152,8 +154,10 @@ class Trainer:
             dtype=torch.bfloat16,
             enabled=(self.config.use_amp),
         ):
-            _, user_repr, impressions, labels = self.model(history, clicks, non_clicks)
-            print(impressions, labels)
+            loss, user_repr, impressions, labels, attn_score, seq_len = self.model(
+                history, clicks, non_clicks
+            )
+            print(user_repr @ impressions.mT, labels)
             indexes, relevance, target = self._prepare_for_metrics(
                 user_repr, impressions, labels
             )
@@ -161,7 +165,6 @@ class Trainer:
                 metric(relevance, target, indexes=indexes) for metric in self.metrices
             ]
             self.optimizer.zero_grad()
-            loss = self.loss_fn(relevance, target)
             if self.config.use_amp:
                 self.scaler.scale(loss).backward()
                 self.scaler.unscale_(self.optimizer)
@@ -198,7 +201,7 @@ class Trainer:
             # if iter % 100 == 0:
             #     current_lr = self.optimizer.param_groups[0]["lr"]
             #     print(
-            #         f"[RANK {self.global_rank}] Step{epoch}:{iter} | {step_type} Loss {batch_loss:.5f} |"
+            #         f"[RANK {self.local_rank}] Step{epoch}:{iter} | {step_type} Loss {batch_loss:.5f} |"
             #         f" auc: {metrices[0]:.5f} | ndcg@5: {metrices[1]:.4f} | ndcg@10: {metrices[2]:.4f} | lr: {current_lr:.6f}"
             #     )
             #     if self.local_rank == 0: